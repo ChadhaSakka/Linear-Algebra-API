f0dc75f2663cc9e5a7fad7d07253489d740622d2
ChadhaSakka <chadha.sakka247@gmail.com>
2023-04-19 16:56:49 +0200

    Hypre API solver

---
+++ HypreExample.h
@@ -0,0 +1,18 @@
+/* --------------------------------
+Chadha SAKKA 
+N° 22205770 
+M1 CHPS 
+POOCS Project
+----------------------------------*/
+
+#ifndef HYPREXAMPLE_H_
+#define HYPREXAMPLE_H_
+
+#include "LocalLinearAlgebra.h"
+
+	class HypreExample {
+	public:
+  	LocalLinearAlgebra::ResidualNorms run();
+  	};
+
+#endif
